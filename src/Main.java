25fcbe34d583068b1c43c3147119c78de3d016a8
Vitaliy <skrepach@gmail.com>
2022-09-12 18:29:16 +0500

    4  commit

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         // add from Local Main
         // add from vit_new_branch without push
         // add 3 commit and try push to remote
+        // add 4 commit in vet_new_branch
+        //new commit
+        //one more
     }
 }
