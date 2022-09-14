df53776e96449edf1a153fb7f7e35ceb70feb25f
Vitaliy <skrepach@gmail.com>
2022-09-14 13:11:13 +0500

    //new commit after  fix main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println( "hi git");
         //add comment
-        //add new commet and merge
+        //add new commit and merge
         // I WANT MERGE FROM 3_MAIN TO MAIN
+        // new commit
+        // and one more
     }
 }
