fb928dea6ab9fd4b28216119a172a322cc025134
Vitaliy <skrepach@gmail.com>
2022-09-14 12:59:15 +0500

    add comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println( "hi git");
-    //add comment
+        //add comment
+        //add new commit and merge
     }
 }
