eb604d3f8068b0584ca988e89a03381fc09c112e
Vitaliy <skrepach@gmail.com>
2022-09-14 12:36:59 +0500

    del comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println( "hi git");
-    // do it from Remote origin
-        // add from Local Main
-        // add from vit_new_branch without push
-        // add 3 commit and try push to remote
-        // chenge in git
 
-        // some new
     }
 }
