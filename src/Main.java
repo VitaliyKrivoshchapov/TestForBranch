f6c40ec31ed62e08bcc17fabb699489b86d3cb8f
Vitaliy <skrepach@gmail.com>
2022-09-21 16:06:22 +0500

    for merge 3_main and main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println( "hi git");
-    //add comment
-        //add new commet and merge
-        // In main you change text
-        // change in main
-        // someboedy shange my project
+//  pr in main
     }
 }
