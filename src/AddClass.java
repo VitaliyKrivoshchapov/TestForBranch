7b244e70533f264cf41cc5bd9964ae4bde327015
Vitaliy <skrepach@gmail.com>
2022-10-14 15:17:37 +0500

    add fix

--- src/AddClass.java
+++ src/AddClass.java
@@ -1,4 +1,9 @@
 public class AddClass {
-
+    public static void main(String[] args) {
+        int i = 4;
+        int rez = 0;
+        rez = --i;
+        System.out.println(rez);
+    }
     //asdfga5vvceed
 }
