c0b1de9eb7e7c957198f0259170bdc3ea78c1f36
Vitaliy <skrepach@gmail.com>
2022-10-21 23:31:56 +0500

    new commit 11 31

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println( "hi git");
+// change all
+    // new  change
+        // do somthing in new Branch
+        
         System.out.println("hi git");
         //slkdfngtwoigsmnrtlwoijfgh[;
         //adfasdf
