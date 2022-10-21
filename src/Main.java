ece6235c2e2908396c6785fa152b0d0d1a19b87d
Vitaliy <skrepach@gmail.com>
2022-10-21 23:30:51 +0500

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "hi git");
+        System.out.println("hi git");
 // from git
 // change all
-    // new  change
+        // new  change
 // add here i will add new lint from idea
-
-
-
+ //1 commit
 
     }
-
-
-
-
 }
