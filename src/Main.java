32705fb9d41eee9582e74ddc1f950333ec2de1b3
Vitaliy <69453241+VitaliyKrivoshchapov@users.noreply.github.com>
2022-10-21 22:48:20 +0500

    Update Main.java
    added new line

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println( "hi git");
-// change all
-    // new  change
-
-
-
-
-
+// from git
     }
-
-
-
-
 }
