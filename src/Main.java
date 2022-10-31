77ac003d90e7910aa6b40e4488fad952db16037c
Vitaliy <skrepach@gmail.com>
2022-10-31 23:40:47 +0500

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi git");
-// from git
-// change all
-        // new  change
-// add here i will add new lint from idea
- //1 commit
-// sombody do in main
-        // and now and now
-        // i was born in Yekaterinburg
-        // and woring in main
-        //do  task more
+
     }
 }
